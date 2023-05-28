4f5feb3621f14ec358f7b4356bc8f28b829d4ae1
Amiraldo Ferreira <102382923+amiralo01@users.noreply.github.com>
2023-05-28 17:33:24 -0300

    Resolução da Atividade 9
    Essa atividade foi resolvida seguindo duas lógicas, uma mais padrão pedindo os 3 números normalmente e adicionando a uma variável, e outra pedindo e adicionando a uma lista.

--- Class_Activities/Atividade9.py
+++ Class_Activities/Atividade9.py
@@ -1,1 +1,32 @@
 """Faça um programa que leia três númeors e mostre o maior e o menor entre eles"""
+
+# Maneira comum:
+
+"""numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+numero3 = int(input('Digite o terceiro número: '))
+
+maior = numero1
+menor = numero1
+
+if numero2 > maior:
+    maior = numero2
+elif numero2 < menor:
+    menor = numero2
+
+if numero3 > maior:
+    maior = numero3
+elif numero3 < menor:
+    menor = numero3
+
+print(f'Maior número = {maior} e menor número = {menor}')"""
+
+# Com listas
+
+numeros = []
+
+for i in range(3):
+    numero = int(input('Digite um número: '))
+    numeros.append(numero)
+
+print(f'O maior número é {max(numeros)} e o menor é {min(numeros)}')
